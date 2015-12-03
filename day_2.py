64a68081ff8303a76dfbb4ec01ac4e3f3da44497
powder-river <dadkisso@live.com>
2015-12-02 23:11:09 -0500

    day two complete

--- day_2.py
+++ day_2.py
@@ -6,10 +6,30 @@ def clean_presents(presents):
     presents = presents.split('\n')
     for p in presents:
          p = p.split('x')
-         clean_list.append({"length":p[0],"width":p[1],"height":p[2]})
+         clean_list.append({"l":int(p[0]),"w":int(p[1]),"h":int(p[2])})
     return clean_list
 
 
+def total_wrapping(presents):
+    new_list = []
+    fabric_totals = []
+    for p in presents:
+        extra_fabric = sorted(
+        [p["l"]*p['w'] , p['w']*p['h'] , p['h']*p['l']]
+    )
+        extra_fabric = extra_fabric[0]
+        total = (2*p["l"]*p['w']) + (2*p['w']*p['h']) + (2*p['h']*p['l'])
+        total = total + extra_fabric
+        new_list.append(total)
 
-def main():
-    pass
+    return sum(new_list)
+
+
+def main(presents):
+    presents = clean_presents(presents)
+
+    presents = total_wrapping(presents)
+
+    return presents
+
+print(main(presents))
